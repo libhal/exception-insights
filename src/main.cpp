48c111b6b74799da87cb859d2bd1595eb41efafc
Mchan2003 <mchan100work@gmail.com>
2025-04-06 22:53:09 -0700

    add elf_parser .hpp with function to open the elf file and read out the elf header
    Completed Section and Program header parsing
    
    implement function to store lsda into a binary file and another to close the elf file
    
    fixed formatting
    
    fixed syntax errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,41 +1,23 @@
-#include "../include/abiparse.hpp"
-#include <fstream>
-#include <vector>
-#include <iostream>
+#include "../include/elf_parser.hpp"
+#include <bitset>
+#include <print>
 #include <string>
-#include <filesystem>
 
-// keep in mind of pathing!!!
-int main(int argc, char** argv) 
+int main(int argc, char** argv)
 {
-    // temporarily reading LSDA file before merging with main
-    const char* path = (argc >= 2 ? argv[1] : "LSDA/lsda");
-    std::ifstream file(path, std::ios::binary);
-    if (!file) 
+    std::string lsda = ".gcc_except_table";
+    std::string symtab = ".symtab";
+    if (argc != 2)
     {
-        path = "LSDA/lsda";
-        file.open(path, std::ios::binary);
-    }
-    if (!file) 
-    {
-        std::cerr << "cannot open LSDA file\n";
-        return 1;
+        errx(EX_USAGE, "usage : %s file - name ", argv[0]);
     }
 
-    std::vector<uint8_t> lsda_data(
-        (std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
-    Abi_parser parser(lsda_data);
+    ElfParser elf(argv);
+    elf.openElf();
+    elf.printShdr();
+    elf.getSection(lsda);
 
-    try 
-    {
-        parser.parse();
-        parser.print_call_sites("LSDA/lsda_output.txt"); // temporary file output
-        parser.print_actions("LSDA/lsda_output.txt"); // temporary file output
-
-    } catch (const std::exception& e) 
-    {
-        std::cerr << "parsing error: " << e.what() << "\n";
-        return 1;
-    }
+    elf.closeElf();
     return 0;
+    ;
 }
