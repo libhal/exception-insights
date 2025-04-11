4f7c13de8bd46c3d435255f26f69c891109b3b09
Mchan2003 <mchan100work@gmail.com>
2025-04-10 17:00:14 -0700

    Completed Section and Program header parsing

--- include/elf_parser.hpp
+++ include/elf_parser.hpp
@@ -3,7 +3,6 @@
 #include <fcntl.h>
 #include <libelf.h> 
 #include <gelf.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <sysexits.h>
 #include <unistd.h>
@@ -12,68 +11,53 @@
 
 using namespace std;
 
-// typedef struct
-// {
-//     unsigned char	e_ident[EI_NIDENT];	/* Magic number and other info */
-//     Elf64_Half	e_type;			/* Object file type */
-//     Elf64_Half	e_machine;		/* Architecture */
-//     Elf64_Word	e_version;		/* Object file version */
-//     Elf64_Addr	e_entry;		/* Entry point virtual address */
-//     Elf64_Off	e_phoff;		/* Program header table file offset */
-//     Elf64_Off	e_shoff;		/* Section header table file offset */
-//     Elf64_Word	e_flags;		/* Processor-specific flags */
-//     Elf64_Half	e_ehsize;		/* ELF header size in bytes */
-//     Elf64_Half	e_phentsize;		/* Program header table entry size */
-//     Elf64_Half	e_phnum;		/* Program header table entry count */
-//     Elf64_Half	e_shentsize;		/* Section header table entry size */
-//     Elf64_Half	e_shnum;		/* Section header table entry count */
-//     Elf64_Half	e_shstrndx;		/* Section header string table index */
-// } Elf64_Ehdr;
-
 class Elf_parser
 {
     public: 
         Elf_parser(char **file_p) : file(file_p) {};
         void openElf ();
         void printEhdr();
-        //getSection_header
-        //getProgram_header
-        //get_lsda
+        void printPhdr();
+        void printShdr();
+        void getlsda(char *section);
         //printElf
     private:
-      int elfClass = 0;
-      int fd;
+        int elfClass = 0;
+        int fd;
+        size_t n, shstrndx;
+        // sz;
 
-      char **file;
+        char **file;
 
-      Elf* e; 
+        char * name;
+        // *p , pc [4* sizeof ( char )];
+        Elf_Scn * scn ;
+        Elf_Data * data ;
 
-      GElf_Ehdr ehdr;
+        Elf* e; 
+        GElf_Ehdr ehdr;
+        GElf_Phdr phdr;
+        GElf_Shdr shdr;
 };
 
 void Elf_parser::openElf()
 {
-    //Checks version of the elf
     if ( elf_version(EV_CURRENT) == EV_NONE ) 
     {
         errx(EX_SOFTWARE , "ELF library initialization failed : %s " , elf_errmsg ( -1));
     }
-    //opens elf file
     if (( fd = open ( file[1] , O_RDONLY , 0)) < 0)
     {
         err (EX_NOINPUT , "open \%s\" failed " , file[1]);
     }
-    //reads elf file
     if (( e = elf_begin ( fd , ELF_C_READ , NULL )) == NULL )
     {
         errx (EX_SOFTWARE , "elf_begin () failed : %s . " , elf_errmsg ( -1)); 
     }
-    //Checks if file is an elf object
     if(elf_kind (e) != ELF_K_ELF)
     {
         errx(EX_DATAERR, "\"%s\" is not an ELF object. ", file[1]);
     }
-    //prints bit version of elf object
     println("{} {}-bit ELF object\n", file[1], elfClass == ELFCLASS32 ? 32 : 64);
 }
 
@@ -103,4 +87,109 @@ void Elf_parser::printEhdr()
     println("shentsize: 0x{:X}", ehdr.e_shentsize);
     println("shnum: 0x{:X}", ehdr.e_shnum);
     println("phnum: 0x{:X}", ehdr.e_phnum);
+}
+
+void Elf_parser::printPhdr()
+{
+    if (elf_getphdrnum (e , &n) != 0) 
+    {
+        errx(EX_SOFTWARE, "getphdrnum() failed: %s.", elf_errmsg(-1));
+    }
+
+    println("Program Header: ({})", static_cast<int>(n));
+    println("====================");
+
+    for(int i = 0; i < static_cast<int>(n); i++)
+    {
+        if (gelf_getphdr (e , i, &phdr ) != &phdr )
+        {
+            errx(EX_SOFTWARE, "getphdr() failed: %s.", elf_errmsg(-1));
+        }
+
+        println("Program Header: {}", i);
+        println("type : 0x{:X}", phdr.p_type);			
+        println("flags : 0x{:X}", phdr.p_flags);		
+        println("offset : 0x{:X}", phdr.p_offset);		
+        println("vaddr : 0x{:X}", phdr.p_vaddr);		
+        println("paddr : 0x{:X}", phdr.p_paddr);		
+        println("filez : 0x{:X}", phdr.p_filesz);		
+        println("memz : 0x{:X}", phdr.p_memsz);		
+        println("align : 0x{:X}", phdr.p_align);
+        println("====================");
+    }		
+}
+
+void Elf_parser::printShdr()
+{
+    if (elf_getshdrstrndx (e , &shstrndx ) != 0) 
+    {
+        errx(EX_SOFTWARE, "getshdrstrndx() failed: %s.", elf_errmsg(-1));
+    }
+
+    scn = NULL;
+
+    println("Section Header: ({})", static_cast<int>(shstrndx));
+    println("====================");
+
+    while((scn = elf_nextscn(e, scn)) != NULL)
+    {
+        if(gelf_getshdr(scn, &shdr ) != &shdr )
+        {
+            errx(EX_SOFTWARE, "getshdr() failed: %s.", elf_errmsg(-1));
+        }
+
+        if((name = elf_strptr(e, shstrndx, shdr.sh_name)) == NULL )
+        {
+            errx(EX_SOFTWARE, "elf_strptr() failed: %s.", elf_errmsg(-1));
+        }
+
+        println("Section {}: {}", static_cast<uintmax_t>(elf_ndxscn(scn)), name );		
+        println("   type : 0x{:X}", shdr.sh_type);		
+        println("   flags : 0x{:X}", shdr.sh_flags);		
+        println("   addr : 0x{:X}", shdr.sh_addr);		
+        println("   offset : 0x{:X}", shdr.sh_offset);		
+        println("   size : 0x{:X}", shdr.sh_size);		
+        println("   link : 0x{:X}", shdr.sh_link);		
+        println("   info : 0x{:X}", shdr.sh_info);
+        println("   addralign : 0x{:X}", shdr.sh_addralign);
+        println("   entsize : 0x{:X}", shdr.sh_entsize);
+        println("====================");
+    }	
+}
+
+void Elf_parser::getlsda(char *section)
+{
+    if (elf_getshdrstrndx (e , &shstrndx ) != 0) 
+    {
+        errx(EX_SOFTWARE, "getshdrstrndx() failed: %s.", elf_errmsg(-1));
+    }
+
+    scn = NULL;
+
+    while((scn = elf_nextscn(e, scn)) != NULL)
+    {
+        if(gelf_getshdr(scn, &shdr ) != &shdr )
+        {
+            errx(EX_SOFTWARE, "getshdr() failed: %s.", elf_errmsg(-1));
+        }
+
+        if((name = elf_strptr(e, shstrndx, shdr.sh_name)) == NULL )
+        {
+            errx(EX_SOFTWARE, "elf_strptr() failed: %s.", elf_errmsg(-1));
+        }
+
+        if(name == section)
+        {
+            break;
+        }
+    }
+    
+    println("Section {}: {}", static_cast<uintmax_t>(elf_ndxscn(scn)), name );	
+    println("====================");
+    data = null;
+    n = 0;
+    while (n < shdr.sh_size && (data = ef_getdata(scn, data)) != NULL)
+    {
+        //TODO (Michael): map the data bytes to LSDA format (See Khalil's slides at 143)
+    }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,8 @@
 
 using namespace std;
 
-//todo: label the outputs
-//todo: read section and program headers
-//todo: read out sections and headers
-//todo: find and extract the lsda at gcc_except_table
-
 int main ( int argc , char **argv )
 {
-    //Checks if number or args are valid
     if ( argc != 2)
     {
         errx(EX_USAGE , "usage : %s file - name " , argv [0]);
@@ -19,6 +13,8 @@ int main ( int argc , char **argv )
 
     Elf_parser elf(argv);
     elf.openElf();
-    elf.printEhdr();
-    exit (EX_OK);
+    // elf.printEhdr();
+    // elf.printPhdr();
+    elf.printShdr();
+    return 0;;
 }
