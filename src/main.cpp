b0135b5e2d579b70a7d9b62e0a008ff123bd8d44
DinoKengi <kennytran018@gmail.com>
2025-04-17 10:37:22 -0700

    added abiparse.hpp and .cpp to parse through dummy

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,22 @@
-#include <print>
+#include "abiparse.hpp"
+#include <fstream>
+#include <vector>
+#include <iostream>
 
-int main()
+int main() 
 {
-    std::println("yeet: {}", __cplusplus);
+    std::ifstream file("lsda.bin", std::ios::binary);
+    if (!file) {
+        std::cerr << "cant open lsda.bin\n";
+        return 1;
+    }
+
+    std::vector<uint8_t> lsda_data((std::istreambuf_iterator<char>(file)), {});
+    Abi_parser parser(lsda_data);
+
+    parser.parse();
+    parser.print_call_sites();
+    parser.print_actions();
+
     return 0;
 }
