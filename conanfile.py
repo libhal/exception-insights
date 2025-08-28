3daa2fa0b3de20b2104ff0e3e5ca8e23376f9632
PhazonicRidley <ma13hew@gmail.com>
2025-08-27 21:24:12 -0700

    :sparkles: testing: Created framework for unit testing

--- conanfile.py
+++ conanfile.py
@@ -28,7 +28,6 @@ class safe_conan(ConanFile):
     format: `self.requires(<package_name_on_conancenter>)`
     """
     def requirements(self):
-        self.requires("libelf/0.8.13")
         self.requires("tl-function-ref/1.0.0")
 
     def generate(self):
@@ -40,6 +39,8 @@ class safe_conan(ConanFile):
     """
     def build_requirements(self):
         self.tool_requires("cmake/[^3.27.1]")
+        self.tool_requires("libhal-cmake-util/[^4.0.5]")
+        self.test_requires("boost-ext-ut/2.1.0")
         self.tool_requires("libhal-cmake-util/[^4.0.5]")
 
     """
