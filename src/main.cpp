9cbe5c9e4fdcd99cd0a7c97440d994b9f84bef1c
DinoKengi <kennytran018@gmail.com>
2025-09-22 18:42:13 -0700

    keeping main after changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,41 @@
 #include <fstream>
 #include <vector>
 #include <iostream>
+#include <string>
+#include <filesystem>
 
 // keep in mind of pathing!!!
-
-int main() 
+int main(int argc, char** argv) 
 {
-    std::ifstream file("LSDA/lsda.bin", std::ios::binary);
-    if (!file) {
-        std::cerr << "cant open lsda.bin\n";
+    // temporarily reading LSDA file before merging with main
+    const char* path = (argc >= 2 ? argv[1] : "LSDA/lsda");
+    std::ifstream file(path, std::ios::binary);
+    if (!file) 
+    {
+        path = "LSDA/lsda";
+        file.open(path, std::ios::binary);
+    }
+    if (!file) 
+    {
+        std::cerr << "cannot open LSDA file\n";
         return 1;
     }
 
-    std::vector<uint8_t> lsda_data((std::istreambuf_iterator<char>(file)), {});
+    std::vector<uint8_t> lsda_data(
+        (std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
     Abi_parser parser(lsda_data);
 
-    parser.parse();
-    parser.print_call_sites();
-    parser.print_actions();
+    try 
+    {
+        parser.parse();
+        parser.print_call_sites("LSDA/lsda_output.txt"); // temporary file output
+        parser.print_actions("LSDA/lsda_output.txt"); // temporary file output
 
+    } catch (const std::exception& e) 
+    {
+        std::cerr << "parsing error: " << e.what() << "\n";
+        return 1;
+    }
     return 0;
 }
 
